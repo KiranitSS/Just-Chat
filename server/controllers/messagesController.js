d955e5a26dfca9610d97c2ec52d15c097dcb3ffd
Wazowskiy <SilverInTrash@protonmail.com>
2024-02-09 16:50:37 +0100

    Server side added to repository.

---
+++ server/controllers/messagesController.js
@@ -0,0 +1,2 @@
+module.exports.addMessage = async (req, res, next) => {};
+module.exports.getAllMessages = async (req, res, next) => {};
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+require("dotenv").config();
+app.use(express.json({limit: '50mb'}));
+
+app.use(cors());
+app.use("/api/auth", userRoutes);
+
+const PORT = parseInt(process.env.PORT);
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("DB connection established")
+    })
+    .catch((err) => {
+        console.log(err.message)
+    });
+
+const server = app.listen(PORT, () => {
+    console.log(`Server Started on Port ${PORT}`);
+});
+
